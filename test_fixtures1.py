f142a673df2569969667a362bc55b988c3dfda77
Amol <amol90304@gmail.com>
2022-09-17 11:12:04 +0530

    second commit

--- test_fixtures1.py
+++ test_fixtures1.py
@@ -1,1 +1,35 @@
-"This is fixture files"
+import pytest
+from selenium import webdriver
+
+driver = None
+
+
+@pytest.fixture
+def setup1():
+    global driver
+    driver = webdriver.Chrome()
+    driver.maximize_window()
+    yield
+    print(driver.title)
+    driver.minimize_window()
+    driver.close()
+
+
+
+def test_demo1(setup1):
+    driver.get("https://google.com")
+
+def test_demo2(setup1):
+    driver.get("https://facebook.com")
+
+def test_demo3(setup1):
+    driver.get("https://amazon.com")
+
+
+
+
+
+
+
+
+
